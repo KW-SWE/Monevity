097a63402098d2d3392a58bbc1424c37f8abfc62
Kevin Wong <kevin.wong@criticalmass.com>
2025-01-22 23:16:40 -0500

    Started RightSidebar

---
+++ app/components/RIghtSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RightSidebar = ({ users, transactions, banks }: RightSidebarProps) => {
+  return (
+    <div>
+      <aside></aside>
+    </div>
+  );
+};
+
+export default RightSidebar;
